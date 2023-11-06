e14a7bc44c719f138a665956a818ae90d5e97bd0
DungeYT <ruben.van.vliet@hva.nl>
2023-11-06 14:43:53 +0100

    sounds+dialogupgrade

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,47 @@
 // JavaScript Document
-console.log("Howdy!");
 
 var areaMountainS = document.querySelector("#area-mount-s");
 var textMountainS = document.querySelector("#text-mount-s");
 var dialogMountainS = document.querySelector("#dialog-mount-s");
+var audioDialog = document.querySelector("body audio:nth-of-type(2)");
 
-areaMountainS.onclick = verhaalMountainS;
-textMountainS.onclick = verhaalMountainS;
+areaMountainS.onclick = dialog1;
+textMountainS.onclick = dialog1;
 
 function verhaalMountainS() {
   dialogMountainS.showModal();
+}
+
+function tanjiroTalking1() {
+  if (audioDialog.paused) {
+    audioDialog.play();
+  } else {
+    audioDialog.pause();
+  }
+}
+function dialog1() {
+  tanjiroTalking1();
+  verhaalMountainS();
+}
+var areaMountainF = document.querySelector("#area-mount-f");
+var textMountainF = document.querySelector("#text-mount-f");
+var dialogMountainF = document.querySelector("#dialog-mount-f");
+
+areaMountainF.onclick = verhaalMountainF;
+textMountainF.onclick = verhaalMountainF;
+
+function verhaalMountainF() {
+  dialogMountainF.showModal();
+}
+var logoImg = document.querySelector("body h1 img");
+var audioTitle = document.querySelector("body audio:nth-of-type(1)");
+
+logoImg.onclick = titleSound;
+
+function titleSound() {
+  if (audioTitle.paused) {
+    audioTitle.play();
+  } else {
+    audioTitle.pause();
+  }
 }
