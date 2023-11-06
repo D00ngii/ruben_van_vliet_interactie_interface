e352dbc00bc9d64d2d9e20f266cacf168877bf23
DungeYT <ruben.van.vliet@hva.nl>
2023-11-06 12:11:08 +0100

    dialog

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,13 @@
 // JavaScript Document
 console.log("Howdy!");
+
+var areaMountainS = document.querySelector("#area-mount-s");
+var textMountainS = document.querySelector("#text-mount-s");
+var dialogMountainS = document.querySelector("#dialog-mount-s");
+
+areaMountainS.onclick = verhaalMountainS;
+textMountainS.onclick = verhaalMountainS;
+
+function verhaalMountainS() {
+  dialogMountainS.showModal();
+}
